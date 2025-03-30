a8a2cb19fd05c2f10dfa43a67a32f900492cbb27
eggboixd <fadhil.revinno@gmail.com>
2025-03-31 00:08:45 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/health', (req, res) => {
+    const currentTime = Date.now(); 
+    const uptime = process.uptime(); 
+  
+    res.status(200).send({
+        "nama": "Fadhil Revinno Hairiman",
+        "nrp": "5025231002",
+        "status": "UP",
+        "timestamp": currentTime,
+        "uptime": uptime
+    });
+});
+
+app.post('/health/:id', (req, res) => {
+    const id = req.params.id; 
+    const { message } = req.body; 
+    const currentTime = Date.now(); 
+    const uptime = process.uptime(); 
+
+    res.status(200).send({
+        "message": message || "bruh" 
+    });
+});
